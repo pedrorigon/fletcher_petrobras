0758e100c75396042933f7f3df2bcee8e5b33afb
pedrorigon <pedrohenriquecasarotto@hotmail.com>
2023-05-23 21:41:23 -0300

    fix

---
+++ exp/compile.sh
@@ -0,0 +1,20 @@
+mkdir -p bin/
+cd ../
+
+for version in original_unified2; do
+	cd $version
+	for backend in CUDA; do
+		echo "-----------------------------------------------------"
+		echo "   $version - $backend"
+		echo "-----------------------------------------------------"
+		make clean
+		make backend=$backend
+	        if [[ $backend == *"OpenACC"* ]]; then
+                        cp ModelagemFletcher.exe ../exp/bin/$version.$backend-CPU.`hostname`.x
+                        mv ModelagemFletcher.exe ../exp/bin/$version.$backend-GPU.`hostname`.x
+		else
+                        mv ModelagemFletcher.exe ../exp/bin/$version.$backend.`hostname`.x
+		fi
+	done
+	cd ..
+done
