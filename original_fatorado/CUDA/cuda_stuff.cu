c8cc14473fd097040044834817e64dad82c2ad2f
pedrorigon <pedrohenriquecasarotto@hotmail.com>
2023-05-27 15:54:23 -0300

    memPrefetchAsync

--- original_fatorado/CUDA/cuda_stuff.cu
+++ original_fatorado/CUDA/cuda_stuff.cu
@@ -33,21 +33,48 @@ void CUDA_Initialize(const int sx, const int sy, const int sz, const int bord,
    int sxsy=sx*sy; // one plan
    const size_t sxsysz=sxsy*sz;
    const size_t msize_vol=sxsysz*sizeof(float);
-//   const size_t msize_vol_extra=msize_vol+2*sxsy*sizeof(float); // 2 extra plans for wave fields
+   const size_t msize_vol_extra=msize_vol+2*sxsy*sizeof(float); // 2 extra plans for wave fields
    
    //arthur -- Se for realizar a cópia assíncrona com prefetch, é aqui o lugar.
+
+   cudaMemPrefetchAsync(ch1dxx, msize_vol, device);
+   cudaMemPrefetchAsync(ch1dyy, msize_vol, device);
+   cudaMemPrefetchAsync(ch1dzz, msize_vol, device);
+   cudaMemPrefetchAsync(ch1dxy, msize_vol, device);
+   cudaMemPrefetchAsync(ch1dyz, msize_vol, device);
+   cudaMemPrefetchAsync(ch1dxz, msize_vol, device);
+   cudaMemPrefetchAsync(v2px, msize_vol, device);
+   cudaMemPrefetchAsync(v2pz, msize_vol, device);
+   cudaMemPrefetchAsync(v2sz, msize_vol, device);
+   cudaMemPrefetchAsync(v2pn, msize_vol, device);
+
+   cudaMemPrefetchAsync(pp, msize_vol_extra, device);
+   cudaMemPrefetchAsync(pc, msize_vol_extra, device);
+   cudaMemPrefetchAsync(qp, msize_vol_extra, device);
+   cudaMemPrefetchAsync(qc, msize_vol_extra, device);
    
    CUDA_CALL(cudaGetLastError());
    CUDA_CALL(cudaDeviceSynchronize());
-   printf("GPU memory usage = %ld MiB\n", 21*msize_vol/1024/1024);
+   printf("GPU memory usage = %ld MiB\n", 15*msize_vol/1024/1024);
+
+   size_t freeMem, totalMem;
+   CUDA_CALL(cudaMemGetInfo(&freeMem, &totalMem));
+   printf("GPU memory usage: %lu MiB (used) / %lu MiB (total)\n", (totalMem - freeMem) / (1024 * 1024), totalMem / (1024 * 1024));
 
 }
 
 
 //ARTHUR - Ajustar função para receber os parametros do CUDA_Finalize.
-void CUDA_Finalize()
+void CUDA_Finalize(const int sx, const int sy, const int sz, const int bord,
+               float dx, float dy, float dz, float dt,
+               float * restrict ch1dxx, float * restrict ch1dyy, float * restrict ch1dzz,
+               float * restrict ch1dxy, float * restrict ch1dyz, float * restrict ch1dxz,
+               float * restrict v2px, float * restrict v2pz, float * restrict v2sz, float * restrict v2pn,
+               float * restrict vpz, float * restrict vsv, float * restrict epsilon, float * restrict delta,
+               float * restrict phi, float * restrict theta,
+               float * restrict pp, float * restrict pc, float * restrict qp, float * restrict qc)
 {
-/*
+   int sxsy=sx*sy; // one plan
    pp-=sxsy;
    pc-=sxsy;
    qp-=sxsy;
@@ -69,11 +96,11 @@ void CUDA_Finalize()
    CUDA_CALL(cudaFree(v2pz));
    CUDA_CALL(cudaFree(v2sz));
    CUDA_CALL(cudaFree(v2pn));
-   CUDA_CALL(cudaFree(pp));
-   CUDA_CALL(cudaFree(pc));
-   CUDA_CALL(cudaFree(qp));
-   CUDA_CALL(cudaFree(qc));
-*/
+   //CUDA_CALL(cudaFree(pp));
+   //CUDA_CALL(cudaFree(pc));
+   //CUDA_CALL(cudaFree(qp));
+   //CUDA_CALL(cudaFree(qc));
+
    printf("CUDA_Finalize: SUCCESS\n");
 }
 
