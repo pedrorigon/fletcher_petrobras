07309aec8ae62a1789cce956d669c2252e6ca3e6
pedrorigon <pedrohenriquecasarotto@hotmail.com>
2023-06-17 18:56:54 -0300

    test

--- original_multiGPU_memCpy/CUDA/cuda_propagate.cu
+++ original_multiGPU_memCpy/CUDA/cuda_propagate.cu
@@ -122,6 +122,8 @@ void CUDA_Propagate(const int sx, const int sy, const int sz, const int bord,
 
         cudaSetDevice(gpu);
 
+        printf("Sou a GPU %d \n", gpu);
+
         if (gpu == 0)
         {
             lower = bord + 1;
