c4e8618e0e43b2ab220f50efd00788486183d7ca
pedrorigon <pedrohenriquecasarotto@hotmail.com>
2023-08-10 20:59:33 -0300

    fix original single

--- original_singleGPU_genetic/model.c
+++ original_singleGPU_genetic/model.c
@@ -9,6 +9,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+#define MEGA 1.0e-6
+#define GIGA 1.0e-9
+
+#ifdef PAPI
+#include "ModPAPI.h"
+#endif
+
+#define MODEL_GLOBALVARS
+#include "precomp.h"
+#undef MODEL_GLOBALVARS
+
 
 #define POPULATION_SIZE 20
 #define MAX_NUM_THREADS 64
@@ -202,108 +213,82 @@ Individuo *gerarNovaSubpopulacao(Individuo *populacao)
 // a aptidão de cada indivíduo será 1/tempo de exec
 
 
-void ReportProblemSizeCSV(const int sx, const int sy, const int sz, const int bord, const int st, FILE *f){
-  fprintf(f,"sx; %d; sy; %d; sz; %d; bord; %d;  st; %d; \n",sx, sy, sz, bord, st);
+void ReportProblemSizeCSV(const int sx, const int sy, const int sz,
+			  const int bord, const int st, 
+			  FILE *f){
+  fprintf(f,
+	  "sx; %d; sy; %d; sz; %d; bord; %d;  st; %d; \n",
+	  sx, sy, sz, bord, st);
 }
 
-void ReportMetricsCSV(double walltime, double MSamples,long HWM, char *HWMUnit, FILE *f){
-  fprintf(f,"walltime; %lf; MSamples; %lf; HWM;  %ld; HWMUnit;  %s;\n",walltime, MSamples, HWM, HWMUnit);
+void ReportMetricsCSV(double walltime, double MSamples,
+		      long HWM, char *HWMUnit, FILE *f){
+  fprintf(f,
+	  "walltime; %lf; MSamples; %lf; HWM;  %ld; HWMUnit;  %s;\n",
+	  walltime, MSamples, HWM, HWMUnit);
 }
 
 
-void Model(const int st, const int iSource, const float dtOutput, SlicePtr sPtr, const int sx, const int sy, const int sz, const int bord,
-           const float dx, const float dy, const float dz, const float dt, const int it, float * restrict pp, float * restrict pc, float * restrict qp, float * restrict qc,
-	         float * restrict vpz, float * restrict vsv, float * restrict epsilon, float * restrict delta, float * restrict phi, float * restrict theta)
+void Model(const int st, const int iSource, const float dtOutput, SlicePtr sPtr, 
+           const int sx, const int sy, const int sz, const int bord,
+           const float dx, const float dy, const float dz, const float dt, const int it, 
+	   float * restrict pp, float * restrict pc, float * restrict qp, float * restrict qc,
+	   float * restrict vpz, float * restrict vsv, float * restrict epsilon, float * restrict delta,
+	   float * restrict phi, float * restrict theta)
 {
 
+  float tSim=0.0;
   int nOut=1;
-  int size = sx*sy*sz;
-  float tSim=0.0, tOut=nOut*dtOutput;
+  float tOut=nOut*dtOutput;
 
-  const long samplesPropagate=(long)(sx-2*(bord))*(long)(sy-2*(bord))*(long)(sz-2*(bord));
+  const long samplesPropagate=(long)(sx-2*bord)*(long)(sy-2*bord)*(long)(sz-2*bord);
   const long totalSamples=samplesPropagate*(long)st;
 
-  float *ch1dxx=NULL;  // isotropy simetry deep angle
-  float *ch1dyy=NULL;  // isotropy simetry deep angle
-  float *ch1dzz=NULL;  // isotropy simetry deep angle
-  float *ch1dxy=NULL;  // isotropy simetry deep angle
-  float *ch1dyz=NULL;  // isotropy simetry deep angle
-  float *ch1dxz=NULL;  // isotropy simetry deep angle
-  float *v2px=NULL;  // coeficient of H2(p)
-  float *v2pz=NULL;  // coeficient of H1(q)
-  float *v2sz=NULL;  // coeficient of H1(p-q) and H2(p-q)
-  float *v2pn=NULL;  // coeficient of H2(p)
-  
-  DRIVER_Allocate_Model_Variables(&ch1dxx, &ch1dyy, &ch1dzz, &ch1dxy, &ch1dyz, &ch1dxz, &v2px, &v2pz, &v2sz, &v2pn, sx, sy, sz);
+#ifdef PAPI
+  long long values[NCOUNTERS];
+  long long ThisValues[NCOUNTERS];
+  for (int i=0; i<NCOUNTERS; i++) {
+    values[i]=0LL;
+    ThisValues[i]=0LL;
+  }
 
-  for (int i=0; i<size; i++) {
-    float sinTheta=sin(theta[i]);
-    float cosTheta=cos(theta[i]);
-    float sin2Theta=sin(2.0*theta[i]);
-    float sinPhi=sin(phi[i]);
-    float cosPhi=cos(phi[i]);
-    float sin2Phi=sin(2.0*phi[i]);
-    ch1dxx[i]=sinTheta*sinTheta * cosPhi*cosPhi;
-    ch1dyy[i]=sinTheta*sinTheta * sinPhi*sinPhi;
-    ch1dzz[i]=cosTheta*cosTheta;
-    ch1dxy[i]=sinTheta*sinTheta * sin2Phi;
-    ch1dyz[i]=sin2Theta         * sinPhi;
-    ch1dxz[i]=sin2Theta         * cosPhi;
-  }
-  #ifdef _DUMP
-  {
-    const int iPrint=ind(bord+1,bord+1,bord+1);
-    printf("ch1dxx=%f; ch1dyy=%f; ch1dzz=%f; ch1dxy=%f; ch1dxz=%f; ch1dyz=%f\n",ch1dxx[iPrint], ch1dyy[iPrint], ch1dzz[iPrint], ch1dxy[iPrint], ch1dxz[iPrint], ch1dyz[iPrint]);
-  }
-  #endif
-
-  // coeficients of H1 and H2 at PDEs
-  for (int i=0; i<size; i++){
-    v2sz[i]=vsv[i]*vsv[i];
-    v2pz[i]=vpz[i]*vpz[i];
-    v2px[i]=v2pz[i]*(1.0+2.0*epsilon[i]);
-    v2pn[i]=v2pz[i]*(1.0+2.0*delta[i]);
-  }
-  
-#ifdef _DUMP
-{
-  const int iPrint=ind(bord+1,bord+1,bord+1);
-  printf("vsv=%e; vpz=%e, v2pz=%e\n", vsv[iPrint], vpz[iPrint], v2pz[iPrint]);
-  printf("v2sz=%e; v2pz=%e, v2px=%e, v2pn=%e\n", v2sz[iPrint], v2pz[iPrint], v2px[iPrint], v2pn[iPrint]);
-}
+  const int eventset=InitPAPI_CreateCounters();
 #endif
 
-  // CUDA_Initialize initialize target, allocate data etc
-DRIVER_Initialize(sx, sy, sz, bord, dx, dy, dz, dt, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, 
-              v2px, v2pz, v2sz, v2pn, vpz, vsv, epsilon, delta, phi, theta, pp, pc, qp, qc); //ok Arthur
+#define MODEL_INITIALIZE
+#include "precomp.h"
+#undef MODEL_INITIALIZE
 
-double walltime=0.0;
-double kernel_time=0.0;
-double res=0.0;
-//int bsize_x=32, bsize_y=16;
+  // DRIVER_Initialize initialize target, allocate data etc
+  DRIVER_Initialize(sx,   sy,   sz,   bord,
+		      dx,  dy,  dz,  dt,
+		      vpz,    vsv,    epsilon,    delta,
+		      phi,    theta,
+		      pp,    pc,    qp,    qc);
 
-Individuo populacao[POPULATION_SIZE];
+  
+  double walltime=0.0;
+  double kernel_time=0.0;
+  double res=0.0;
+  Individuo populacao[POPULATION_SIZE];
+  inicializarPopulacao(populacao, POPULATION_SIZE);
 
-// Inicializar população
-inicializarPopulacao(populacao, POPULATION_SIZE);
+  for (int it=1; it<=st; it++) {
 
-// Loop para executar o kernel com cada configuração e coletar os tempos de execução
-
-for (int it=1; it<=st; it++) {
     // Calculate / obtain source value on i timestep
     float src = Source(dt, it-1);
-    DRIVER_InsertSource(src, iSource, pc, qc, pp, qp);
+    DRIVER_InsertSource(dt,it-1,iSource,pc,qc,src);
 
-   // printf("\nBsize_x: %d \n", bsize_x);
-  //  printf("Bsize_y: %d \n", bsize_y);
-    //update_bsize_values(&bsize_x, &bsize_y, timeIt);
+#ifdef PAPI
+    StartCounters(eventset);
+#endif
 
     if (it <= POPULATION_SIZE)
         {
             printf("\nBsize_x: %d \n", populacao[it - 1].thread_x);
             printf("Bsize_y: %d \n", populacao[it - 1].thread_y);
             const double t0=wtime();
-            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, v2px, v2pz, v2sz, v2pn, pp, pc, qp, qc, populacao[it - 1].thread_x, populacao[it - 1].thread_y);
+            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, pp, pc, qp, qc, populacao[it - 1].thread_x, populacao[it - 1].thread_y);
             kernel_time=wtime()-t0;
             walltime+=kernel_time;
             res = (MEGA*(double)samplesPropagate)/kernel_time;
@@ -326,12 +311,12 @@ for (int it=1; it<=st; it++) {
                 //free(novaSubpopulacao);
             }
         }
-        else if (it <= POPULATION_SIZE * 2)
+    else if (it <= POPULATION_SIZE * 2)
         {
             printf("\nBsize_x: %d \n", populacao[it - POPULATION_SIZE - 1].thread_x);
             printf("Bsize_y: %d \n", populacao[it - POPULATION_SIZE - 1].thread_y);
             const double t1=wtime();
-            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, v2px, v2pz, v2sz, v2pn, pp, pc, qp, qc, populacao[it - POPULATION_SIZE - 1].thread_x, populacao[it - POPULATION_SIZE - 1].thread_y);
+            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, pp, pc, qp, qc, populacao[it - POPULATION_SIZE - 1].thread_x, populacao[it - POPULATION_SIZE - 1].thread_y);
             kernel_time=wtime()-t1;
             walltime+=kernel_time;
             populacao[it - POPULATION_SIZE - 1].fitness = 1 / kernel_time;
@@ -354,12 +339,12 @@ for (int it=1; it<=st; it++) {
                 //free(novaSubpopulacao);
             }
         }
-        else if (it <= POPULATION_SIZE * 3)
+    else if (it <= POPULATION_SIZE * 3)
         {
             printf("\nBsize_x: %d \n", populacao[it - 1 - 2 * POPULATION_SIZE].thread_x);
             printf("Bsize_y: %d \n", populacao[it - 1 - 2 * POPULATION_SIZE].thread_y);
             const double t2=wtime();
-            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, v2px, v2pz, v2sz, v2pn, pp, pc, qp, qc, populacao[it - 1 - 2 * POPULATION_SIZE].thread_x, populacao[it - 1 - 2 * POPULATION_SIZE].thread_y); 
+            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, pp, pc, qp, qc, populacao[it - 1 - 2 * POPULATION_SIZE].thread_x, populacao[it - 1 - 2 * POPULATION_SIZE].thread_y); 
             kernel_time=wtime()-t2;
             walltime+=kernel_time;
             populacao[it - 1 - 2 * POPULATION_SIZE].fitness = 1 / kernel_time;
@@ -381,44 +366,48 @@ for (int it=1; it<=st; it++) {
                 //free(novaSubpopulacao);
             }
         }
-        else
+    else
         {
             int melhorConfig = encontrarMelhorIndice(populacao);
             printf("\nBsize_x: %d \n", populacao[melhorConfig].thread_x);
             printf("Bsize_y: %d \n", populacao[melhorConfig].thread_y);
             const double t3=wtime();
-            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, v2px, v2pz, v2sz, v2pn, pp, pc, qp, qc, populacao[melhorConfig].thread_x, populacao[melhorConfig].thread_y); 
+            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, pp, pc, qp, qc, populacao[melhorConfig].thread_x, populacao[melhorConfig].thread_y); 
             kernel_time=wtime()-t3;
             walltime+=kernel_time;
             printf("MELHOR CONFIG: %d x %d", populacao[melhorConfig].thread_x, populacao[melhorConfig].thread_y);
             res = (MEGA*(double)samplesPropagate)/kernel_time;
             printf("\nIteracao: %d ; Bsize_x: %d ; Bsize_y: %d ; tempoExec: %lf ; MSamples: %lf \n", it, populacao[melhorConfig].thread_x, populacao[melhorConfig].thread_y, kernel_time, res);
         }
-
     //const double t0=wtime();
-    
-    //DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, v2px, v2pz, v2sz, v2pn, pp, pc, qp, qc, bsize_x, bsize_y); //ajustar parametros
+    //DRIVER_Propagate(  sx,   sy,   sz,   bord,
+		//       dx,   dy,   dz,   dt,   it,
+		//       pp,    pc,    qp,    qc);
+
     //SwapArrays(&pp, &pc, &qp, &qc);
+    //walltime+=wtime()-t0;
 
-    //timeIt=wtime()-t0;
-    //walltime+=timeIt;
-
-    //printf("tempo deu: %lf\n", timeIt);
+#ifdef PAPI
+    StopReadCounters(eventset, ThisValues);
+    for (int i=0; i<NCOUNTERS; i++) {
+      values[i]+=ThisValues[i];
+    }
+#endif
 
     tSim=it*dt;
     if (tSim >= tOut) {
+
       //DRIVER_Update_pointers(sx,sy,sz,pc);
       //DumpSliceFile(sx,sy,sz,pc,sPtr);
-    //  CUDA_prefetch_pc(sx,sy,sz,pc);
-
       tOut=(++nOut)*dtOutput;
 #ifdef _DUMP
       //DRIVER_Update_pointers(sx,sy,sz,pc);
-      //DumpSliceSummary(sx,sy,sz,sPtr,dt,it,pc,src);
+      //      DumpSliceSummary(sx,sy,sz,sPtr,dt,it,pc,src);
 #endif
     }
   }
 
+  // get HWM data
 
   const char StringHWM[6]="VmHWM";
   char line[256], title[12],HWMUnit[8];
@@ -433,7 +422,6 @@ for (int it=1; it<=st; it++) {
     }
   }
   fclose(fp);
-  //nao vamos salvar em disco
 
   // Dump Execution Metrics
   
@@ -449,19 +437,27 @@ for (int it=1; it<=st; it++) {
 
   // report problem size
 
-  ReportProblemSizeCSV(sx, sy, sz, bord, st, fr);
+  ReportProblemSizeCSV(sx, sy, sz,
+		       bord, st, 
+		       fr);
 
   // report collected metrics
 
-  ReportMetricsCSV(walltime, MSamples, HWM, HWMUnit, fr);
+  ReportMetricsCSV(walltime, MSamples,
+		   HWM, HWMUnit, fr);
+  
+  // report PAPI metrics
+
+#ifdef PAPI
+  ReportRawCountersCSV (values, fr);
+#endif
   
   fclose(fr);
 
   fflush(stdout);
 
   // DRIVER_Finalize deallocate data, clean-up things etc 
-  DRIVER_Finalize(sx, sy, sz, bord, dx, dy, dz, dt, ch1dxx, ch1dyy, ch1dzz, ch1dxy, ch1dyz, ch1dxz, 
-              v2px, v2pz, v2sz, v2pn, vpz, vsv, epsilon, delta, phi, theta, pp, pc, qp, qc);
+  DRIVER_Finalize();
 
 }
 
