4946ac90fc14ca5d657e671f7926cc25e9691edf
Brenda Schussler <83243250+brendaschussler@users.noreply.github.com>
2023-10-17 01:01:40 -0300

    Update test_framework_genetic.c funcionando

--- original_singleGPU_genetic_data/test_framework_genetic.c
+++ original_singleGPU_genetic_data/test_framework_genetic.c
@@ -1,9 +1,31 @@
-#define POPULATION_SIZE 30
+#include "utils.h"
+#include "source.h"
+#include "driver.h"
+#include "fletcher.h"
+#include "walltime.h"
+#include "model.h"
+#include "CUDA/cuda_stuff.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#define MEGA 1.0e-6
+#define GIGA 1.0e-9
+
+#ifdef PAPI
+#include "ModPAPI.h"
+#endif
+
+#define MODEL_GLOBALVARS
+#include "precomp.h"
+#undef MODEL_GLOBALVARS
+
 #define NUM_GENERATIONS 3 
+#define POPULATION_SIZE 30
 #define MAX_NUM_THREADS 64
 #define MAX_MULTIPLICATION 1024
-#define TOURNAMENT_SIZE 2   //define numero de participantes no torneio 
-#define MUTATION_Y_PROBABILITY 0.2
+#define TOURNAMENT_SIZE 2
+#define MUTATION_Y_PROBABILITY 0.1
 #define MUTATION_X_PROBABILITY 0.0 
 
 typedef struct
@@ -13,10 +35,16 @@ typedef struct
     double fitness;
 } Individuo;
 
-//int isPowerOfTwo(int num)
-//{
-    //return (num & (num - 1)) == 0;
-//}
+
+typedef struct {
+    int bsize_x;
+    int bsize_y;
+} BlockSizes;
+
+int isPowerOfTwo(int num)
+{
+    return (num & (num - 1)) == 0;
+}
 
 void inicializarPopulacao(Individuo *populacao, int tamanho_populacao)
 {
@@ -84,7 +112,7 @@ void selecaoPorTorneio(Individuo *populacao, Individuo *pais)
 {
     for (int i = 0; i < 2; i++)
     {
-        Individuo vencedor = populacao[rand() % POPULATION_SIZE]; //Escolhe um indivíduo aleatório como o primeiro para ser o atual vencedor do torneio
+        Individuo vencedor = populacao[rand() % POPULATION_SIZE]; // Escolhe um indivíduo aleatório como o primeiro candidato vencedor do torneio
 
         for (int j = 1; j < TOURNAMENT_SIZE; j++)
         {
@@ -116,15 +144,14 @@ int gerarNovoValorAleatorio()
 
 
 void crossoverEMutacao(Individuo *pais, Individuo *filhos)
-{ 
-    // Verificar se a multiplicação dos valores é inferior a 1024
-    if ((pais[0].thread_x * pais[1].thread_y < 1024) && (pais[1].thread_x * pais[0].thread_y < 1024))
+{ // Verificar se a multiplicação dos valores de thread_x * thread_y é inferior a 1024
+    if ((pais[1].thread_y * pais[0].thread_x < 1024) && (pais[1].thread_x * pais[0].thread_y < 1024))
     {
         // Realizar crossover trocando os valores entre os pais
-        filhos[0].thread_x = pais[0].thread_x;
+        filhos[0].thread_x = pais[1].thread_x;
         filhos[0].thread_y = pais[1].thread_y;
 
-        filhos[1].thread_x = pais[1].thread_x;
+        filhos[1].thread_x = pais[0].thread_x;
         filhos[1].thread_y = pais[0].thread_y;
     }
     else
@@ -136,28 +163,33 @@ void crossoverEMutacao(Individuo *pais, Individuo *filhos)
     }
 
     // Realizar mutação com probabilidade MUTATION_PROBABILITY
+    // for (int i = 0; i < 2; i++)
+    //{
     if ((double)rand() / RAND_MAX < MUTATION_Y_PROBABILITY)
+{
+    int new_thread_y = gerarNovoValorAleatorio();
+
+    // Garantir que o novo valor de thread_y é válido para mutação
+    while (new_thread_y * filhos[0].thread_x >= MAX_MULTIPLICATION)
     {
-      
-      int new_thread_y = gerarNovoValorAleatorio();
-      // Garantir que o novo valor de thread_y é válido para mutação
-      while (new_thread_y * filhos[0].thread_x >= MAX_MULTIPLICATION)
-      {
-          new_thread_y = gerarNovoValorAleatorio();
-      }
-      filhos[0].thread_y = new_thread_y;
+        new_thread_y = gerarNovoValorAleatorio();
     }
 
-    if ((double)rand() / RAND_MAX < MUTATION_X_PROBABILITY)
-    {
-        int new_thread_x = gerarNovoValorAleatorio();
+    filhos[0].thread_y = new_thread_y;
+}
+
+if ((double)rand() / RAND_MAX < MUTATION_X_PROBABILITY)
+{
+    int new_thread_x = gerarNovoValorAleatorio();
    
-        while (new_thread_x * filhos[0].thread_y >= MAX_MULTIPLICATION)
-        {
-            new_thread_x = gerarNovoValorAleatorio();
-        }
-        filhos[0].thread_x = new_thread_x;
+    while (new_thread_x * filhos[0].thread_y >= MAX_MULTIPLICATION)
+    {
+        new_thread_x = gerarNovoValorAleatorio();
     }
+
+    filhos[0].thread_x = new_thread_x;
+}
+    //}
 }
 
 Individuo *gerarNovaSubpopulacao(Individuo *populacao)
@@ -182,7 +214,7 @@ Individuo *gerarNovaSubpopulacao(Individuo *populacao)
         }
         else
         {
-            novaSubpopulacao[i] = filhos[0]; 
+            novaSubpopulacao[i] = filhos[0]; // Por exemplo, você pode escolher o primeiro filho
         }
     }
 
@@ -196,47 +228,258 @@ Individuo *gerarNovaSubpopulacao(Individuo *populacao)
 
     return populacao; // Retornar a população atualizada
 }
+// preciso rodar o kernel com todas as configurações da população inicial
+// a aptidão de cada indivíduo será 1/tempo de exec
 
-//aqui comeca a ideia 
+Individuo populacao[POPULATION_SIZE]; //global?
 
-int initialize(int it){
-  Individuo populacao[POPULATION_SIZE];
+BlockSizes initialize(int *geracao){
+  //Individuo populacao[POPULATION_SIZE];
   inicializarPopulacao(populacao, POPULATION_SIZE);
+  BlockSizes blockSizes;
+  *geracao = 0;
 
-  int bsize_x = populacao[it - geracao*POPULATION_SIZE - 1].thread_x;
-  int bsize_y = populacao[it - geracao*POPULATION_SIZE - 1].thread_y;
+  blockSizes.bsize_x = populacao[1 - (*geracao)*POPULATION_SIZE - 1].thread_x;
+  blockSizes.bsize_y = populacao[1 - (*geracao)*POPULATION_SIZE - 1].thread_y;
 
-  return bsize_x, bsize_y;
+  return blockSizes;
 }
 
-//executa o kernel com bsize x e y 
-//coleta o tempo desse kernel 
+BlockSizes genetic_exec(int it, double kernel_time, int *geracao){
 
-void genetic_exec(int it, double kernel_time, int bsize_x, int bsize_y){
+  BlockSizes blockSizes;
 
-  if(it % POPULATION_SIZE != 0 && it <= POPULATION_SIZE*3){
-      populacao[it - geracao*POPULATION_SIZE - 1].fitness = 1 / kernel_time; //calcula o fitness da exec anterior
-      bsize_x = populacao[it - geracao*POPULATION_SIZE - 1].thread_x;    //passa config p prox exec
-      bsize_y = populacao[it - geracao*POPULATION_SIZE - 1].thread_y;
+  if(it % POPULATION_SIZE != 0 && it <= POPULATION_SIZE*NUM_GENERATIONS){
+      populacao[it - (*geracao) *POPULATION_SIZE - 1].fitness = 1 / kernel_time; //calcula o fitness da exec anterior
+      blockSizes.bsize_x = populacao[it - (*geracao)*POPULATION_SIZE - 1].thread_x;    //passa config p prox exec
+      blockSizes.bsize_y = populacao[it - (*geracao)*POPULATION_SIZE - 1].thread_y;
+      return blockSizes;
   }
   else if(it % POPULATION_SIZE == 0 && it <= POPULATION_SIZE*3){
     //fazer selecao 
-    geracao = geracao + 1; //fazer ponteiro para pegar o valor atualizado 
+    (*geracao) = (*geracao) + 1; //fazer ponteiro para pegar o valor atualizado 
     Individuo *novaSubpopulacao = gerarNovaSubpopulacao(populacao);
     // Substituir a população atual pela nova subpopulação
     for (int j = 0; j < POPULATION_SIZE; j++){
       populacao[j] = novaSubpopulacao[j];              
     }
+    printf("chegou aqui");
+    blockSizes.bsize_x = populacao[0].thread_x;    //passa config p prox exec
+    blockSizes.bsize_y = populacao[0].thread_y;
+    printf("%d, %d", blockSizes.bsize_x, blockSizes.bsize_y);
+    return blockSizes;
   }
-  else{
+  else {
     //já achou melhor, só segue executando 
     int melhorConfig = encontrarMelhorIndice(populacao);
-    bsize_x = populacao[melhorConfig].thread_x;
-    bsize_y = populacao[melhorConfig].thread_y;
+    blockSizes.bsize_x = populacao[melhorConfig].thread_x;
+    blockSizes.bsize_y = populacao[melhorConfig].thread_y;
+    return blockSizes;
   }
 }
 
 
+void ReportProblemSizeCSV(const int sx, const int sy, const int sz,
+			  const int bord, const int st, 
+			  FILE *f){
+  fprintf(f,
+	  "sx; %d; sy; %d; sz; %d; bord; %d;  st; %d; \n",
+	  sx, sy, sz, bord, st);
+}
+
+void ReportMetricsCSV(double walltime, double MSamples,
+		      long HWM, char *HWMUnit, FILE *f){
+  fprintf(f,
+	  "walltime; %lf; MSamples; %lf; HWM;  %ld; HWMUnit;  %s;\n",
+	  walltime, MSamples, HWM, HWMUnit);
+}
 
 
+void Model(const int st, const int iSource, const float dtOutput, SlicePtr sPtr, 
+           const int sx, const int sy, const int sz, const int bord,
+           const float dx, const float dy, const float dz, const float dt, const int it, 
+	   float * restrict pp, float * restrict pc, float * restrict qp, float * restrict qc,
+	   float * restrict vpz, float * restrict vsv, float * restrict epsilon, float * restrict delta,
+	   float * restrict phi, float * restrict theta)
+{
 
+  float tSim=0.0;
+  int nOut=1;
+  float tOut=nOut*dtOutput;
+
+  const long samplesPropagate=(long)(sx-2*bord)*(long)(sy-2*bord)*(long)(sz-2*bord);
+  const long totalSamples=samplesPropagate*(long)st;
+
+#ifdef PAPI
+  long long values[NCOUNTERS];
+  long long ThisValues[NCOUNTERS];
+  for (int i=0; i<NCOUNTERS; i++) {
+    values[i]=0LL;
+    ThisValues[i]=0LL;
+  }
+
+  const int eventset=InitPAPI_CreateCounters();
+#endif
+
+#define MODEL_INITIALIZE
+#include "precomp.h"
+#undef MODEL_INITIALIZE
+
+  // DRIVER_Initialize initialize target, allocate data etc
+  DRIVER_Initialize(sx,   sy,   sz,   bord,
+		      dx,  dy,  dz,  dt,
+		      vpz,    vsv,    epsilon,    delta,
+		      phi,    theta,
+		      pp,    pc,    qp,    qc);
+
+  
+  double walltime=0.0;
+  double genetic_time=0.0;
+  double kernel_time=0.0;
+  double res=0.0;
+  int geracao;  
+  BlockSizes bsizes = initialize(&geracao);
+ 
+  for (int it=1; it<=st; it++) {
+
+ // Calculate / obtain source value on i timestep
+    float src = Source(dt, it-1);
+    DRIVER_InsertSource(dt,it-1,iSource,pc,qc,src);
+
+#ifdef PAPI
+    StartCounters(eventset);
+#endif
+	
+            printf("\nBsize_x: %d \n", bsizes.bsize_x);
+            printf("Bsize_y: %d \n", bsizes.bsize_y);
+            const double t0=wtime();
+            DRIVER_Propagate(sx, sy, sz, bord, dx, dy, dz, dt, it, pp, pc, qp, qc, bsizes.bsize_x, bsizes.bsize_y);
+            kernel_time=wtime()-t0;
+            walltime+=kernel_time;
+	    genetic_time+=kernel_time;
+            res = (MEGA*(double)samplesPropagate)/kernel_time;
+            printf("\n Iteracao: %d ; Bsize_x: %d ; Bsize_y: %d ; tempoExec: %lf ; MSamples: %lf \n", it, bsizes.bsize_x, bsizes.bsize_y, kernel_time, res);
+	    bsizes = genetic_exec(it, kernel_time, &geracao);
+            // printf("Indivíduo %d: thread_x = %d, thread_y = %d, fitness = %f\n", i - 1, populacao[i - 1].thread_x, populacao[i - 1].thread_y, populacao[i - 1].fitness);
+            //  printf("Indivíduo %d: thread_x = %d, thread_y = %d, fitness = %f\n", i, populacao[i - 1].thread_x, populacao[i - 1].thread_y, populacao[i - 1].fitness);
+            //    inicio primeira seleção genética
+            //     Gerar a nova subpopulação
+           //const double t0=wtime();
+    //DRIVER_Propagate(  sx,   sy,   sz,   bord,
+		//       dx,   dy,   dz,   dt,   it,
+		//       pp,    pc,    qp,    qc);
+
+    //SwapArrays(&pp, &pc, &qp, &qc);
+    //walltime+=wtime()-t0;
+
+#ifdef PAPI
+    StopReadCounters(eventset, ThisValues);
+    for (int i=0; i<NCOUNTERS; i++) {
+      values[i]+=ThisValues[i];
+    }
+#endif
+
+    tSim=it*dt;
+    if (tSim >= tOut) {
+
+      //DRIVER_Update_pointers(sx,sy,sz,pc);
+      //DumpSliceFile(sx,sy,sz,pc,sPtr);
+      tOut=(++nOut)*dtOutput;
+#ifdef _DUMP
+      //DRIVER_Update_pointers(sx,sy,sz,pc);
+      //      DumpSliceSummary(sx,sy,sz,sPtr,dt,it,pc,src);
+#endif
+    }
+  }
+
+  // get HWM data
+
+  const char StringHWM[6]="VmHWM";
+  char line[256], title[12],HWMUnit[8];
+  const long HWM;
+  const double MSamples=(MEGA*(double)totalSamples)/walltime;
+  
+  FILE *fp=fopen("/proc/self/status","r");
+  while (fgets(line, 256, fp) != NULL){
+    if (strncmp(line, StringHWM, 5) == 0) {
+      sscanf(line+6,"%ld %s", &HWM, HWMUnit);
+      break;
+    }
+  }
+  fclose(fp);
+
+  // Dump Execution Metrics
+  
+  printf ("Execution time (s) is %lf\n", walltime);
+  printf ("Genetic time (s) is %lf\n", genetic_time);
+  printf ("Genetic time PORCENTAGEM %lf\n", genetic_time*100/walltime);
+  printf ("MSamples/s %.0lf\n", MSamples);
+  printf ("Memory High Water Mark is %ld %s\n",HWM, HWMUnit);
+
+  // FILE *outputFile = fopen("output.txt", "w");
+
+   //if (outputFile == NULL) {
+     //  printf("Erro ao abrir o arquivo de saída.\n");
+      // return 1;
+  // }
+
+   // Escrever os valores no arquivo
+  // fprintf(outputFile, "walltime: %lf\n", walltime);
+  // fprintf(outputFile, "genetic_time: %lf\n", genetic_time);
+  // fprintf(outputFile, "MSamples: %.0lf\n", MSamples);
+
+   // Fechar o arquivo
+  // fclose(outputFile);
+  // Criar uma string formatada para o nome do arquivo
+    char filename[5]; // Ajuste o tamanho conforme necessário
+    sprintf(filename, "output%d.txt", sx-40); // "output%d.txt" é o formato do nome do arquivo
+
+    // Abrir o arquivo com o nome formatado
+    FILE *outputFile = fopen(filename, "w");
+
+    if (outputFile == NULL) {
+        printf("Erro ao abrir o arquivo de saída.\n");
+        return 1;
+    }
+
+    // Escrever os valores no arquivo
+    fprintf(outputFile, "walltime: %lf\n", walltime);
+    fprintf(outputFile, "genetic_time: %lf\n", genetic_time);
+    fprintf(outputFile, "Genetic time PORCENTAGEM %lf\n", genetic_time*100/walltime); 
+    fprintf(outputFile, "MSamples: %.0lf\n", MSamples);
+
+    // Fechar o arquivo
+    fclose(outputFile);
+  
+
+// Dump Execution Metrics in CSV
+  
+  FILE *fr=NULL;
+  const char fName[]="Report.csv";
+  fr=fopen(fName,"w");
+
+  // report problem size
+
+  ReportProblemSizeCSV(sx, sy, sz,
+		       bord, st, 
+		       fr);
+
+  // report collected metrics
+
+  ReportMetricsCSV(walltime, MSamples,
+		   HWM, HWMUnit, fr);
+  
+  // report PAPI metrics
+
+#ifdef PAPI
+  ReportRawCountersCSV (values, fr);
+#endif
+  
+  fclose(fr);
+
+  fflush(stdout);
+
+  // DRIVER_Finalize deallocate data, clean-up things etc 
+  DRIVER_Finalize();
+
+}
